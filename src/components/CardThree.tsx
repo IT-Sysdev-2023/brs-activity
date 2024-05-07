716832ed3ec8de972044f8c8582d00a3b807f72f
San103 <palbanjessan15@gmail.com>
2024-05-07 16:02:49 +0800

    rename stroke-line to strokeLine as terminal recommend it

--- src/components/CardThree.tsx
+++ src/components/CardThree.tsx
@@ -29,20 +29,20 @@ const CardThree = () => {
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             {' '}
             <path
               d="M3 21.0001H21M4 18.0001H20M6 18.0001V13.0001M10 18.0001V13.0001M14 18.0001V13.0001M18 18.0001V13.0001M12 7.00695L12.0074 7.00022M21 10.0001L14.126 3.88986C13.3737 3.2212 12.9976 2.88688 12.5732 2.75991C12.1992 2.64806 11.8008 2.64806 11.4268 2.75991C11.0024 2.88688 10.6263 3.2212 9.87404 3.88986L3 10.0001H21Z"
               stroke="#3C50E0"
-              stroke-width="1.224"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.224"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>{' '}
           </g>
         </svg>
